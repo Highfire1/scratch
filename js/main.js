07239ca895584d3d9cd93d39e8c5a6a3994d7faf
Anderson T <tseng.andersonn@gmail.com>
2023-06-13 19:33:46 -0700

    more minimizing (hope it isn't too much)

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
-var calendarClass
-
 // initialize the calendar
 document.addEventListener('DOMContentLoaded', async function() {
-    var calendarElement = document.getElementById('calendar');
-    start_date = "2023-9-05"
+    
+    date = "2023-9-05"
+    initial_date = new Date(new Date(date).getTime() + 604800000)
 
     try {
+      var calendarElement = document.getElementById('calendar');
+
       var FCalendar = new FullCalendar.Calendar(calendarElement, {
         schedulerLicenseKey: 'CC-Attribution-NonCommercial-NoDerivatives',
         rerenderDelay: 10,
@@ -14,13 +15,12 @@ document.addEventListener('DOMContentLoaded', async function() {
         timeZone: 'America/Vancouver',
         initialView: 'timeGridWeek', // 'resourceTimelineDay'
 
-        initialDate: new Date(new Date(start_date).getTime() + 604800000), // start on the second week of courses
-
+        initialDate: initial_date, // start on the second week of courses
       })
 
       FCalendar.render();
+
     } catch (error) {
       alert(error)
-
     }
 });
